02fba069907d365540b8c20d33340b2dc3bf5d38
jenahmarie <jenahmarie28@gmail.com>
2024-11-11 11:11:15 +0800

    revised admin dashboard

---
+++ resources/views/layouts/admin/admin_sidebar_layout.blade.php
@@ -0,0 +1,160 @@
+ <!-- Sidebar -->
+ <div class="w-64 bg-black text-white min-h-screen">
+    <div class="flex items-center justify-center h-16 bg-gray-900">
+     <span class="text-xl font-bold">
+      AdminLTE 3
+     </span>
+    </div>
+    <div class="flex items-center p-4">
+     <img alt="User profile picture" class="rounded-full w-10 h-10" height="40" src="https://storage.googleapis.com/a1aa/image/H0VCNwmSjQ51OJmjf6eTG0ad6mGTp9cGe15teCuOJBWJbHfdC.jpg" width="40"/>
+     <span class="ml-4">
+      Alexander Pierce
+     </span>
+    </div>
+    <div class="p-4">
+     <div class="relative">
+      <input class="w-full p-2 rounded bg-gray-700 text-white" placeholder="Search" type="text"/>
+      <i class="fas fa-search absolute top-3 right-3 text-gray-400">
+      </i>
+     </div>
+    </div>
+    <nav class="p-4">
+     <ul>
+      <li class="mb-2">
+       <a class="flex items-center p-2 bg-blue-600 rounded" href="#">
+        <i class="fas fa-tachometer-alt mr-2">
+        </i>
+        Dashboard
+        <i class="fas fa-angle-down ml-auto">
+        </i>
+       </a>
+       <ul class="pl-6 mt-2">
+        <li class="mb-2">
+         <a class="flex items-center p-2 text-gray-400" href="#">
+          <i class="far fa-circle mr-2">
+          </i>
+          Dashboard v1
+         </a>
+        </li>
+        <li class="mb-2">
+         <a class="flex items-center p-2 text-gray-400" href="#">
+          <i class="far fa-circle mr-2">
+          </i>
+          Dashboard v2
+         </a>
+        </li>
+        <li>
+         <a class="flex items-center p-2 bg-gray-300 text-gray-800 rounded" href="#">
+          <i class="far fa-circle mr-2">
+          </i>
+          Dashboard v3
+         </a>
+        </li>
+       </ul>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center p-2 text-gray-400" href="#">
+        <i class="fas fa-th mr-2">
+        </i>
+        Widgets
+        <span class="ml-auto bg-red-500 text-white text-xs rounded px-2">
+         New
+        </span>
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center p-2 text-gray-400" href="#">
+        <i class="fas fa-copy mr-2">
+        </i>
+        Layout Options
+        <span class="ml-auto bg-teal-500 text-white text-xs rounded px-2">
+         6
+        </span>
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center p-2 text-gray-400" href="#">
+        <i class="fas fa-chart-pie mr-2">
+        </i>
+        Charts
+       </a>
+      </li>
+      <li class="mb-2">
+       <h3 class="text-sm font-semibold">
+        EXAMPLES
+       </h3>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-calendar-alt mr-2">
+        </i>
+        Calendar
+        <span class="ml-auto bg-blue-500 text-white text-xs rounded-full px-2">
+         2
+        </span>
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-image mr-2">
+        </i>
+        Gallery
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-columns mr-2">
+        </i>
+        Kanban Board
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-envelope mr-2">
+        </i>
+        Mailbox
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-file-alt mr-2">
+        </i>
+        Pages
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-plus mr-2">
+        </i>
+        Extras
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-search mr-2">
+        </i>
+        Search
+       </a>
+      </li>
+      <li class="mb-2">
+       <h3 class="text-sm font-semibold">
+        MISCELLANEOUS
+       </h3>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-plug mr-2">
+        </i>
+        Tabbed IFrame Plugin
+       </a>
+      </li>
+      <li class="mb-2">
+       <a class="flex items-center" href="#">
+        <i class="fas fa-book mr-2">
+        </i>
+        Documentation
+       </a>
+      </li>
+     </ul>
+    </nav>
+   </div>
